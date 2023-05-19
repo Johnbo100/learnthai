45ae54fbe96bb52d33a0cc9f1606e7fffe32a802
John Jones <johnkeartonjones@googlemail.com>
2023-05-19 22:51:33 +0700

    Cleared git cache

--- src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Title() {
-  return (
-    <div className='title'>English <span className='yeltitle'>to</span> <span id="thai">Thai</span> <span className='yeltitle'>Flash Cards</span></div>
-  )
-}
-
-export default Title
